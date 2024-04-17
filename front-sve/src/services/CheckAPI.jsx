b5b81a064e0a4be0d9bdec636347b4b5a70b1051
Gabriel Ecker <gabriel@diamaju.local>
2024-04-17 11:25:40 -0300

    alteração na lógica do api check, para apenas fazer a requisição ao clicar o botão

--- front-sve/src/services/CheckAPI.jsx
+++ front-sve/src/services/CheckAPI.jsx
@@ -1,26 +1,34 @@
-/* eslint-disable no-unused-vars */
 import { DisconnectOutlined, ApiOutlined } from '@ant-design/icons';
-import { useEffect, useState } from 'react';
-import { api_db } from '../services/api'
+import { useContext, useEffect, useState } from 'react';
+import { api_db } from '../services/api';
 import { Button, Tooltip } from 'antd';
+import { DataContext } from '../context/DataContext';
 
 export const StatusButton = () => {
-    const [apiStatus, setApiStatus] = useState('')
-    
-    useEffect(() => {
-        async function checkApi() {
-            try {
-                const response = await api_db.get("/status")
-                setApiStatus(response.status)
-            } catch (err) {
-                setApiStatus('500')
-            }
-        } checkApi();
-    }, [])
+    const { apiChecked, setApiChecked } = useContext(DataContext);
+    const [apiStatus, setApiStatus] = useState(null);  
+
+    const checkApi = async () => {
+        try {
+            const response = await api_db.get("/status");
+            setApiStatus(response.status);
+        } catch (err) {
+            setApiStatus(500); // Defina para 500 em caso de erro
+        } finally {
+            setApiChecked(true);
+        }
+    };
+
+    const handleButtonClick = () => {
+        checkApi(false);
+    };
+
+    // Define a cor padrão do botão com base no estado apiStatus
+    const buttonColor = apiStatus === 200 ? 'yellowgreen' : apiStatus === 500 ? 'red' : 'white';
 
     return (
-        <Tooltip title={apiStatus === 200 ? "Serviço de busca está online!" : "Serviço de busca está offline!"}
-            color={apiStatus === 200 ? 'green' : 'red'}
+        <Tooltip title={apiStatus === 200 ? "Serviço de busca está online!" : apiStatus === 500 ? "Erro ao verificar serviço de busca!" : "Clique para verificar o status do serviço de busca"}
+            color={apiStatus === 200 ? 'green' : apiStatus === 500 ? 'red' : 'grey'}
             placement='left'>
             <Button
                 size="lg"
@@ -32,7 +40,7 @@ export const StatusButton = () => {
                     position: 'absolute',
                     top: '15px',
                     right: '135px',
-                    color: apiStatus === 200 ? 'yellowgreen' : 'red',
+                    color: buttonColor, // Texto branco para visibilidade
                     border: 'none',
                     borderRadius: '50%',
                     width: '30px',
@@ -40,8 +48,9 @@ export const StatusButton = () => {
                     padding: 0,
                 }}
                 type="primary"
-                shape='circle' />
+                shape='circle'
+                onClick={handleButtonClick}
+            />
         </Tooltip>
-
-    )
-}
+    );
+};
